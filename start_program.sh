c49a8340e15e536f18c076baffcd4a97d3e7f367
Nikola Vlaskalin <nikola.vlaskalin00@gmail.com>
2023-06-06 00:05:30 +0200

    Fixed bash script to correctly name output files

--- start_program.sh
+++ start_program.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#bash start_program.sh datasets/train/images datasets/out "Augmentation 4" 1.2 /home/nikolavlaskalin/Desktop/ADOS_PZ1_Labeling_and_Augmentation/datasets/train/labels 4 3 175 3
+#bash start_program.sh datasets/train/images datasets/out "Augmentation 4" 1.2 /home/nikolavlaskalin/Desktop/dsp2-projekat1-2023/datasets/train/labels 4 3 175 3
 
 #Set input and output directory of images
 input_dir="$1"
@@ -13,6 +13,29 @@ asp2=$7
 histogram_argument=$8
 num_of_occlusions=$9
 
+#Set augmentation type {0, 1, 2, 3, 4, all)
+augmentatio_type=$3
+
+if [ "$3" = "Augmentation 0" ]
+then
+    augmentation_type="a0"
+elif [ "$3" = "Augmentation 1" ]
+then
+    augmentation_type="a1"
+elif [ "$3" = "Augmentation 2" ]
+then
+    augmentation_type="a2"
+elif [ "$3" = "Augmentation 3" ]
+then
+    augmentation_type="a3"
+elif [ "$3" = "Augmentation 4" ]
+then
+    augmentation_type="a4"
+elif [ "$4" = "Augmentation all" ]
+then
+    augmentation_type="aa"
+fi
+
 #exposure start_x start_y end_x end y asp1 asp2 
 for file in "$input_dir"/*.jpg; do
     echo "Doing file = $file"
@@ -20,6 +43,7 @@ for file in "$input_dir"/*.jpg; do
     input_img="$input_dir/$filename.jpg"
     output_img=""$output_dir/$filename"_"$augmentation_type".jpg"
     label_file="$label_dir/$filename.txt"
+        echo $output_img
     echo "Doing label file = $label_file"
     echo "Doing augmentation = $3"
     if [[ -f "$label_file" ]]; then
